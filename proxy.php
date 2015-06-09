3e6acbc9c439f2c9b6f4123de3822b701c1daef6
Tomas Banzas Illa <tomaban@gmail.com>
2015-06-09 18:25:19 +0200

    proxy.php caching

--- proxy.php
+++ proxy.php
@@ -1,11 +1,32 @@
 <?php
+
+
 $url = $_GET['requrl'];
+
 if(strpos($url, '.png') !== false){
     $filename = substr(strrchr($url, "/"), 1);
 
     echo file_get_contents("tex/".$filename);
+}else if(strpos($url, '.obj') !== false or (strpos($url, '.mtl') !== false)){
+	$needle = ':3000/';
+	$filename = substr(strstr($url, $needle), strlen($needle));
+
+	if(file_exists("cache/".$filename)){
+		echo file_get_contents($url);
+	}else{
+		$folder = "cache/". substr($filename, 0,strrpos($filename, '/'));
+		//die( var_dump ($folder));	
+		if (!file_exists($folder)) {
+			// folder isn't a dir
+			mkdir($folder,'0777', true);
+		}
+		
+		file_put_contents("cache/".$filename, fopen($url, 'r'));
+		echo file_get_contents($url);
+	}
+	
+   
 }else{
-    $file = file_get_contents($url);
-    echo $file;
+	echo file_get_contents($url);
 }
 ?>
